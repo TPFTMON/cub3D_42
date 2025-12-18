0d8f7302a3a1eb7f9ac9d63a0ad95eef0330da31
TPFTMON <tpftmoon@gmail.com>
2025-12-18 15:07:40 +0100

    engine: the game_loop is being updated. Almost it's final form

--- build/sources/engine/game_loop.c
+++ build/sources/engine/game_loop.c
@@ -25,7 +25,7 @@ int update_player(t_cube *cube){
 
     //    1.2. Calculate delta_time
 	//    setting delta_time and converting to seconds instead of ms:
-	cube->delta_time = ((current_time - cube->last_frame_time) / 1000);
+	cube->delta_time = ((current_time - cube->last_frame_time) / 1000.0);
 
 	//    1.3. Set effective move/rot speed from it
 	effective_move_speed = cube->player.move_speed * cube->delta_time;
@@ -40,7 +40,7 @@ int update_player(t_cube *cube){
     //    3.1.
 }
 
-int raycaster(t_cube *cube){
+int perform_raycasting(t_cube *cube){
     // 1. Calculate ray vectors
     //    1.1.
 
@@ -49,10 +49,8 @@ int raycaster(t_cube *cube){
 
     // 3. Calculate distance
     //    3.1.
-}
 
-int render_pixels(t_cube *cube){
-    // Draw the pixels
+    // 4. Draw the pixels
 }
 
 int	game_loop(void *arg)
@@ -65,13 +63,74 @@ int	game_loop(void *arg)
 	update_player(cube);
 
     // 2. Do the raycasting:
-    raycaster(cube);
+    perform_raycasting(cube);
 
-	// 3. Render all pixels (ceiling, floor, walls (later textures))
-	render_pixels(cube);
-
-	// 4. Put the ready image to the window
+	// 3. Put the ready image to the window
 	mlx_put_image_to_window(cube->mlx_ptr, cube->win_ptr, cube->screen.img_ptr, 0, 0);
 
 	return (0);
 }
+
+// int update_player(t_cube *cube){
+// 	long long	current_time;
+// 	double		effective_move_speed;
+// 	double		effective_rot_speed;
+
+//     // 1. Relative movement updates
+// 	//    1.1. Update the last_frame_time for smooth movement
+// 	if (cube->last_frame_time == 0)
+// 		cube->last_frame_time = get_time_in_ms();
+// 	current_time = get_time_in_ms();
+
+//     //    1.2. Calculate delta_time
+// 	//    setting delta_time and converting to seconds instead of ms:
+// 	cube->delta_time = ((current_time - cube->last_frame_time) / 1000.0);
+
+// 	//    1.3. Set effective move/rot speed from it
+// 	effective_move_speed = cube->player.move_speed * cube->delta_time;
+// 	effective_rot_speed = cube->player.rot_speed * cube->delta_time;
+
+//     // 2. Update player's position
+//     //    update_positon():
+//     //    2.1.
+
+//     // 3. Update player's point of view
+//     //    update_view():
+//     //    3.1.
+// }
+
+// int perform_raycasting(t_cube *cube){
+//     // 1. Calculate ray vectors
+//     //    1.1.
+
+//     // 2. Run the DDA (to find the wall)
+//     //    2.1.
+
+//     // 3. Calculate distance
+//     //    3.1.
+// }
+
+// int render_pixels(t_cube *cube){
+//     // Draw the pixels
+// }
+
+// int	game_loop(void *arg)
+// {
+// 	t_cube		*cube;
+
+// 	cube = (t_cube *)arg;
+
+// 	// 1. Update the logic (move player based on relative time and the keys pressed)
+// 	update_player(cube);
+
+//     // 2. Do the raycasting:
+//     perform_raycasting(cube);
+
+// 	// 3. Render all pixels (ceiling, floor, walls (later textures))
+// 	render_pixels(cube);
+
+// 	// 4. Put the ready image to the window
+// 	mlx_put_image_to_window(cube->mlx_ptr, cube->win_ptr, cube->screen.img_ptr, 0, 0);
+
+// 	return (0);
+// }
